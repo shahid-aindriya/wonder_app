f43b2132644f122a82238560a7de1ef4fa9912a6
mds115577 <122812733+shahid-aindriya@users.noreply.github.com>
2023-03-30 02:16:52 +0530

    fspeech recognition remaining

--- lib/app/data/urls.dart
+++ lib/app/data/urls.dart
@@ -1,8 +1,8 @@
 import 'package:get/get.dart';
 
-final baseUrl = "http://64.227.178.175:8000/".obs;
+final baseUrl = "https://wonderpoints.com/".obs;
 final headers = {"Content-Type": "application/json"}.obs;
-final baseUrlForImage = "http://64.227.178.175:8000";
+final baseUrlForImage = "https://wonderpoints.com/";
 // http://64.227.178.175:8000/
 // http://64.227.156.53:8000/
 // https://wonderpoints.com/
